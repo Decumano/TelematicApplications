ede4ba1071541460809f0c69c7ccd3f6f13fed60
Decumano <marcgg171000@gmail.com>
2023-01-30 17:54:54 +0100

    Seminar 3
    Seminar 3 completed

---
+++ Seminar 3/Servlet.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Servlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        String docType = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">\n";
+        out.println
+        (
+            docType +
+            "<html>\n" +
+            " <head><title>Test Servlet</title></head>\n" +
+            "<body>\n" +
+            " <h1>Hello!</h1>\n" +
+            "</body></html>"
+        );
+    }
+}
