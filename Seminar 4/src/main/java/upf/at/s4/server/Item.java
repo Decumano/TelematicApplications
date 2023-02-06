af11b80fe3a1946d6780596c2e2c42f2ee5e5eec
Decumano <marcgg171000@gmail.com>
2023-02-06 12:31:01 +0100

    Seminar 4 update
    Implementation of Maven and more code for Seminar 4

---
+++ Seminar 4/src/main/java/upf/at/s4/server/Item.java
@@ -0,0 +1,53 @@
+package upf.at.s4.server;
+
+
+public class Item {
+    private String name;
+    private int price;
+    private int quantity;
+
+    public Item ()
+    {
+        name = "";
+        price = 0;
+        quantity = 0;
+    }
+
+    public Item (String n, int p, int q)
+    {
+        name = n;
+        price = p;
+        quantity = q;
+    }
+
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String toString()
+    {
+        return "Product name: " + name + "\nPrice: " + price + "\nQuantity " + quantity + "\n";
+    }
+
+}
---
+++ Seminar 4/src/main/java/upf/at/s4/server/Services.java
@@ -0,0 +1,55 @@
+package upf.at.s4.server;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+
+@Path("/item")
+public class Services {
+
+    static List<Item> items = new ArrayList<>();
+
+    static Item error404 = new Item("error 404: Item not found", 404, 1);
+    
+    @POST
+    @Path("/add")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response create(Item item)
+    {
+        items.add(item);
+        return Response.status(200).entity(item).build();
+    }
+
+    @GET
+    @Path("/get/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Item get(@PathParam("name") String name)
+    {
+        for (Item item : items)
+        {
+            if(item.getName().equalsIgnoreCase(name))
+            {
+                return item;
+            }
+        }
+        return error404;
+    }
+
+    @GET
+    @Path("/get/all")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Item> get()
+    {
+        return items;
+    }
+}
